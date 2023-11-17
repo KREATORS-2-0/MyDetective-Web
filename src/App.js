2d0448e88950384e183c19acc2df252f1ad65a35
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-11-17 00:36:33 -0700

    work

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Button from "./components/Button";
 import UnorderedList from "./components/UnorderedList";
 import Image from "./components/ImageComponent";
 import "./css/app.css";
+import './css/background.css';
+import './css/header.css';
 import Connect from "./components/Connect";
+
 
 const App = () => {
   return (
     <>
+    <div className="app-container">
+    <h1 className="my-detective-heading">MyDetective</h1>
+    </div>
+     <div className="background-image-container">
       <Connect />
-      <div className="app-container">
-        <h1>MyDetective</h1>
-      </div>
       <UnorderedList></UnorderedList>
       <Image />
       <Button />
-    </>
+      </div>
+      </>
   );
 };
 
