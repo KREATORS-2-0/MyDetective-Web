a3a14e7f67da7413a6268ef5efc447c293207c6d
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-11-17 23:45:19 -0700

    changed

--- src/gpt.js
+++ src/gpt.js
@@ -1,4 +1,4 @@
-const openaiApiKey = "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"; // Load API key from environment variable
+const openaiApiKey = "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"; // Load API key from environment variable
 
 async function getCompletion(
   prompt,
@@ -40,11 +40,14 @@ export default getCompletion;
 // Example usage
 (async () => {
   let conversationHistory = [];
+ //Detective Prompt
+  const prompt1 = `The suspect name is ${name} born at ${date}. The suspect is a ${relationship} 
+  to the victim. The cases summarizes that ${caseSummary}. The evidence that supports the crime is ${caseEvidence}.
+  the suspect has criminal records of ${crimeRecords}. Provide me some guide questions to ask in 1 sentence for continuing interrogation`
 
-  const prompt1 = "Hi, my name is Taekwan. Nice to meet you!";
   let response = await getCompletion(prompt1, conversationHistory);
   console.log(response);
-
+  // Gets data
   const prompt2 = "Do you know my name?";
   response = await getCompletion(prompt2, conversationHistory);
   console.log(response);
